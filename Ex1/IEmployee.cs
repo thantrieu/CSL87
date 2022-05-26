b82caf2e40dac196daab20e8ba85be88526cf09e
Trieu Than <trieuthan92pro@gmail.com>
2022-05-26 10:24:45 +0700

    Add exercises 1 + 2 lesson 8.7

---
+++ Ex1/IEmployee.cs
@@ -0,0 +1,10 @@
+﻿namespace L87Exercises1
+{
+    // interface chỉ ra các hành động của từng sinh viên nói chung
+    interface IEmployee
+    {
+        void CheckIn(string time);
+        void CheckOut(string time);
+        long CalculateSalary(long profit = 0);
+    }
+}
---
+++ Ex1/IFilter.cs
@@ -0,0 +1,11 @@
+﻿namespace L87Exercises1
+{
+    interface IFilter
+    {
+        bool IsNameValid(string name);
+        bool IsEmailValid(string email);
+        bool IsPhoneValid(string phone);
+        bool IsEmpIdValid(string empId);
+        bool IsDateFormatValid(string dateStr);
+    }
+}
